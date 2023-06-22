98013e3f459ade40467c95e5e9f41b7ef38dec1e
Akash Chattikal <20308@burnside.school.nz>
2023-06-22 12:40:57 +1200

    Drop Down Menu
    Basic drop down menu completed.

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ const navLinks = document.querySelectorAll('.nav_link');
 
 navLinks.forEach(navLink => {
     navLink.addEventListener('click', () => {
-        document.querySelector('.btn')?.classList.remove('btn');
-        navLink.classList.add('btn');
+        document.querySelector('.btn')?.classList.remove('.btn');
+        navLink.classList.add('.btn');
     });
 });
